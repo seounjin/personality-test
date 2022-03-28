c56cec1982c6843c81169af64c93c8e3868d592e
qustj <invanda7@gmail.com>
2022-03-28 13:38:36 +0900

    commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,7 @@ import { CustomRoute } from './types';
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
+const session = require('express-session');	
 
 app.use(bodyParser.urlencoded({extended:true})); 
 
@@ -18,6 +19,17 @@ app.use(
     credentials: true,
   }),
 )
+
+app.use(session({
+  httpOnly: true,	//자바스크립트를 통해 세션 쿠키를 사용할 수 없도록 함
+  secret: 'secret key',	//암호화하는 데 쓰일 키
+  resave: false,	//세션을 언제나 저장할지 설정함
+  saveUninitialized:true,
+  cookie: {	//세션 쿠키 설정 (세션 관리 시 클라이언트에 보내는 쿠키)
+    httpOnly: true,
+    Secure: true
+  }
+}));
 
 app.use('/static', express.static(__dirname + '/public'));
 
